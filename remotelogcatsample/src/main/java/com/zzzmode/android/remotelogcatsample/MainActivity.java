8965120ad0277327c1549a21aa8f8dbed853b3ef
喵叔catuncle <andych008@163.com>
2018-08-13 13:00:47 +0800

    show ws address and update it autoly when wifi state changed.

--- remotelogcatsample/src/main/java/com/zzzmode/android/remotelogcatsample/MainActivity.java
+++ remotelogcatsample/src/main/java/com/zzzmode/android/remotelogcatsample/MainActivity.java
@@ -1,10 +1,16 @@
 package com.zzzmode.android.remotelogcatsample;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import android.widget.TextView;
 import com.zzzmode.android.remotelogcat.LogcatRunner;
 
 import java.io.IOException;
@@ -15,11 +21,21 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     
     private boolean logRunning = false;
+    private TextView textIP;
+    private WifiReceiver wifiReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textIP = findViewById(R.id.textIP);
+
+        wifiReceiver = new WifiReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(wifiReceiver, filter);
 
         try {
             LogcatRunner.getInstance()
@@ -83,8 +99,35 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+        unregisterReceiver(wifiReceiver);
         LogcatRunner.getInstance().stop();
+        super.onDestroy();
 
+    }
+
+
+    public class WifiReceiver extends BroadcastReceiver {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (WifiManager.WIFI_STATE_CHANGED_ACTION.equals(action)
+                || WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)) {
+
+
+                WifiManager wm = (WifiManager) context.getApplicationContext().getSystemService(context.WIFI_SERVICE);
+                int ipAddressInt = wm.getConnectionInfo().getIpAddress();
+                String ipAddress = String.format("%d.%d.%d.%d", (ipAddressInt & 0xff), (ipAddressInt >> 8 & 0xff), (ipAddressInt >> 16 & 0xff), (ipAddressInt >> 24 & 0xff));
+
+                StringBuilder sbf = new StringBuilder();
+                sbf.append("ws://")
+                    .append(ipAddress)
+                    .append(":")
+                    .append(LogcatRunner.getInstance().getPort())
+                    .append(LogcatRunner.getInstance().getWsPrefix());
+                textIP.setText(sbf);
+
+                Log.i("testlog", "" + sbf);
+            }
+        }
     }
 }
